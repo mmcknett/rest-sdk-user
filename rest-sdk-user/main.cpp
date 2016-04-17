1b645ebc25baa5e911312a78b6ac6372de17706e
Matt McKnett <mmcknett@gmail.com>
2016-04-16 21:41:16 -0700

    Initial Commit

---
+++ rest-sdk-user/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rest-sdk-user
+//
+//  Created by Matt McKnett on 4/16/16.
+//  Copyright © 2016 Stronghold. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
