ce3c3000ac0422eeb60f7f8c713096081772a571
Matt McKnett <mmcknett@gmail.com>
2016-04-16 21:46:45 -0700

    Making a small change to try it out.

--- rest-sdk-user/main.cpp
+++ rest-sdk-user/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!  " << 1 << std::endl;
     return 0;
 }
